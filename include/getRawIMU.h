40b4778a50846b0ef4d7f872d5b1d7f36e12b015
rlacodmskim <112742002+rlacodmskim@users.noreply.github.com>
2023-01-12 22:16:29 +0900

    Create getRawIMU.h

---
+++ include/getRawIMU.h
@@ -0,0 +1,22 @@
+#include "../include/calibrateIMU.h"
+
+// getting raw data function
+// get accelerator raw data
+t_accVal getAccRawContinue(){
+  t_accVal ret;
+  ret.accX = (Wire.read() << 8 | Wire.read()) / LSB_Sensitivity_R4_2;
+  ret.accY = (Wire.read() << 8 | Wire.read()) / LSB_Sensitivity_R4_2;
+  ret.accZ = (Wire.read() << 8 | Wire.read()) / LSB_Sensitivity_R4_2;
+
+  return ret;
+} // ret에 채워 넣은 걸 반환하겠다
+
+// get raw gyroscope data
+t_gyroVal getGyroRawContinue(){
+  t_gyroVal ret;
+  ret.gyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
+  ret.gyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+  ret.gyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+
+  return ret;
+} // t_gyroVal는 반환 형태임
